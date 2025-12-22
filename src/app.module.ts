70e8a6ecfe1284f7049e2f6b381d0405c21e652e
Ibrahim Tajudeen <donslice6@gmail.com>
2025-12-22 13:38:20 +0100

    add: sales grenerate

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SupabaseModule } from './supabase/supabase.module';
+import { SalesModule } from './sales/sales.module';
 import { SupabaseModule } from './supabase/supabase.module';
 
 @Module({
-  imports: [SupabaseModule],
+  imports: [SupabaseModule, SalesModule],
   controllers: [AppController],
   providers: [AppService],
 })
