f8eb9260affa1d44451dc932edb3706c44cccf17
Ibrahim Tajudeen <donslice6@gmail.com>
2025-12-22 13:37:26 +0100

    add: supabase grenerate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SupabaseModule } from './supabase/supabase.module';
+import { SupabaseModule } from './supabase/supabase.module';
 
 @Module({
-  imports: [],
+  imports: [SupabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
