3b9f42c55406b11b6834f73d399a7b5659d59715
Ibrahim Tajudeen <donslice6@gmail.com>
2025-12-22 13:39:18 +0100

    add: reports grenerate

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SupabaseModule } from './supabase/supabase.module';
+import { ReportsModule } from './reports/reports.module';
 import { SalesModule } from './sales/sales.module';
 import { SupabaseModule } from './supabase/supabase.module';
 
 @Module({
-  imports: [SupabaseModule, SalesModule],
+  imports: [SupabaseModule, SalesModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
