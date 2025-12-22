8b0fec2dcecb519942ab36a682a580bc848af804
Ibrahim Tajudeen <donslice6@gmail.com>
2025-12-22 14:35:15 +0100

    add: role decorator

--- src/common/decorators/role.decorator.ts
+++ src/common/decorators/role.decorator.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { SetMetadata } from '@nestjs/common';
-import { UserRole } from 'src/common/types/user-role.types';
+import { UserRole } from 'src/common/types/user-role.type';
 
 export const ROLES_KEY = 'roles';
 export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
