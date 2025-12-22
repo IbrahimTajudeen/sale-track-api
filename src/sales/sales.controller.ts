d9e2b0ca1c8b478d7de627d610c02d4c8bf5fd6b
Ibrahim Tajudeen <donslice6@gmail.com>
2025-12-22 14:14:05 +0100

    add: sale controller

--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -1,34 +1,40 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+// sales/sales.controller.ts
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Put,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 import { SalesService } from './sales.service';
 import { CreateSaleDto } from './dto/create-sale.dto';
 import { UpdateSaleDto } from './dto/update-sale.dto';
+import { SupabaseAuthGuard } from '../common/guards/supabase-auth.guard';
+import { User } from '../common/decorators/user.decorator';
 
 @Controller('sales')
+@UseGuards(SupabaseAuthGuard)
 export class SalesController {
   constructor(private readonly salesService: SalesService) {}
 
   @Post()
-  create(@Body() createSaleDto: CreateSaleDto) {
-    return this.salesService.create(createSaleDto);
+  create(@User() user, @Body() dto: CreateSaleDto) {
+    return this.salesService.create(user.id, dto);
   }
 
   @Get()
-  findAll() {
-    return this.salesService.findAll();
+  findAll(@User() user) {
+    return this.salesService.findAll(user.id);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.salesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSaleDto: UpdateSaleDto) {
-    return this.salesService.update(+id, updateSaleDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.salesService.remove(+id);
+  @Put(':id')
+  update(
+    @User() user,
+    @Param('id') id: string,
+    @Body() dto: UpdateSaleDto,
+  ) {
+    return this.salesService.update(user.id, id, dto);
   }
 }
