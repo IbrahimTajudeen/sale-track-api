876dfcc6de180fd81b4ac5848fce13319d32ee26
Ibrahim Tajudeen <donslice6@gmail.com>
2025-12-22 14:10:57 +0100

    add: sale dto

--- src/sales/dto/create-sale.dto.ts
+++ src/sales/dto/create-sale.dto.ts
@@ -1,1 +1,18 @@
-export class CreateSaleDto {}
+// sales/dto/create-sale.dto.ts
+import { IsDateString, IsInt, IsNumber, IsString, Min } from 'class-validator';
+
+export class CreateSaleDto {
+  @IsDateString()
+  saleDate: string;
+
+  @IsString()
+  itemName: string;
+
+  @IsNumber()
+  @Min(0)
+  pricePerItem: number;
+
+  @IsInt()
+  @Min(1)
+  quantity: number;
+}
